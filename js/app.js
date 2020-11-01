4e9551774fff2af1b038bb824883f88f36fd08f5
Juho Kangas <juhokn@gmail.com>
2020-11-01 11:38:53 +0200

    UI rework and add basic functionality

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-    
+    $('.btn-number').click(function(){
+        if($('#numOne').val().length < 5) {
+            $('#numOne').val ($('#numOne').val() + $(this).val());
+        } 
+    })
+
+    $('.btn-operator').click(function(){
+        $('#numTwo').focus();
+    })
 });
