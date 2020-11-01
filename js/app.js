781a3127eed341a1a1c733aa558f9e140e28c9e3
Juho Kangas <juhokn@gmail.com>
2020-11-01 12:39:20 +0200

    Add the full functionality to the calculator

--- js/app.js
+++ js/app.js
@@ -1,11 +1,63 @@
 $(document).ready(function(){
-    $('.btn-number').click(function(){
-        if($('#numOne').val().length < 5) {
-            $('#numOne').val ($('#numOne').val() + $(this).val());
-        } 
-    })
 
+    //INITIALIZING THE APP AND RESETTING ALL THE VALUES ON START/RELOAD
+    var firstNumFocus = true;
+    var operator = '';
+    $('#numOne').val('');
+    $('#numTwo').val('');
+    $('#result').val('');
+    
+
+    //NUMBER BUTTON HANDLER
+    $('.btn-number').click(function(){
+        if(firstNumFocus){
+            //Limiting the input length to 5
+            if($('#numOne').val().length < 5) {
+                $('#numOne').val ($('#numOne').val() + $(this).val());
+            } 
+        } else {
+            if($('#numTwo').val().length < 5) {
+                $('#numTwo').val ($('#numTwo').val() + $(this).val());
+            }
+        }
+    });
+
+
+    //OPERATOR BUTTON HANDLER
     $('.btn-operator').click(function(){
+        operator = $(this).attr('id');
+        $('#operator').html($(this).html());
         $('#numTwo').focus();
-    })
+        firstNumFocus = false;
+    });
+
+    //CALCULATE BUTTON HANDLER
+    $('#calc').click(function(){
+        var calcResult;
+
+        if (operator === 'plus') {
+            calcResult = parseInt($('#numOne').val()) + parseInt($('#numTwo').val())
+            $('#result').val(calcResult) 
+        } else if (operator === 'minus'){
+            calcResult = parseInt($('#numOne').val()) - parseInt($('#numTwo').val())
+            $('#result').val(calcResult) 
+        } else if (operator === 'times') {
+            calcResult = parseInt($('#numOne').val()) * parseInt($('#numTwo').val())
+            $('#result').val(calcResult) 
+        } else if (operator === 'divide') {
+            calcResult = parseInt($('#numOne').val()) / parseInt($('#numTwo').val())
+            $('#result').val(calcResult) 
+        }
+    });
+
+    //Handle user specified focus to input numbers correctly and empty previous inputs
+    $('#numOne').focus(function(){
+        $('#numOne').val('');
+        firstNumFocus = true;
+    });
+
+    $('#numTwo').focus(function(){
+        $('#numTwo').val('');
+        firstNumFocus = false;
+    });
 });
